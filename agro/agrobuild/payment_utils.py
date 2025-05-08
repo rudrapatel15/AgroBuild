34c774d50f5c1c6faa63cf03f96d765abd37b5af
Rudra Patel <rp3041536@gmail.com>
2025-05-08 07:10:01 +0530

    error solve

--- agro/agrobuild/payment_utils.py
+++ agro/agrobuild/payment_utils.py
@@ -1,14 +1,13 @@
-# # payment_utils.py
-# import razorpay
-# from django.conf import settings
+import razorpay
+from django.conf import settings
 
-# client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
 
-# def create_razorpay_order(amount, currency='INR'):
-#     data = {
-#         'amount': int(amount * 100),  # Razorpay expects amount in paise
-#         'currency': currency,
-#         'payment_capture': 1  # Auto-capture payment
-#     }
-#     order = client.order.create(data=data)
-#     return order
+def create_razorpay_order(amount, currency='INR'):
+     data = {
+         'amount': int(amount * 100),  # Razorpay expects amount in paise
+         'currency': currency,
+         'payment_capture': 1  # Auto-capture payment
+     }
+     order = client.order.create(data=data)
+     return order
