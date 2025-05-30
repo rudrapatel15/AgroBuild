78810383ce57e32e6157baccf4073e5e9fabf89c
Rudra Patel <rp3041536@gmail.com>
2025-05-31 01:37:24 +0530

    message

--- agro/agrobuild/management/commands/send_watering_reminder.py
+++ agro/agrobuild/management/commands/send_watering_reminder.py
@@ -16,12 +16,11 @@ def get_season():
     else:
         return 'winter'
 
-# Helper: get timedelta for frequency string
 def get_timedelta_for_frequency(freq):
     if freq == 'daily':
         return timedelta(days=1)
     elif freq == 'twice':
-        return timedelta(hours=12)  # twice a day
+        return timedelta(hours=12)
     elif freq == 'every_two_days':
         return timedelta(days=2)
     elif freq == 'weekly':
@@ -41,11 +40,9 @@ class Command(BaseCommand):
             if not delivered_orders.exists():
                 continue
 
-            # For each plant in this category, check its watering frequency for this season
-            notified_products = []
             for order in delivered_orders:
                 if order.payment_status != 'paid':
-                   continue  # Only process paid orders
+                    continue
                 for item in order.items.filter(product__categories=reminder.category):
                     product = item.product
                     # Get frequency for this season
@@ -59,39 +56,78 @@ class Command(BaseCommand):
                         freq = product.monsoon_watering
                         times = [reminder.morning_time]
 
-                    # Check if it's time to send for this product
-                    should_send = False
-                    # For "twice" frequency, check both times; for others, just morning_time
                     for t in times:
                         # Only send if last_sent is None or enough time has passed
                         if not reminder.last_sent or (now - reminder.last_sent) >= get_timedelta_for_frequency(freq):
                             # Check if current time is within 5 minutes of the scheduled time
                             if abs((now.time().hour * 60 + now.time().minute) - (t.hour * 60 + t.minute)) < 5:
-                                should_send = True
+                                is_morning = (t.hour < 15)
+                                customer_name = reminder.user.first_name or reminder.user.username
+                                plant_name = product.P_name
+                                subject = ""
+                                msg_plain = ""
+                                msg_html = ""
+                                if is_morning:
+                                    subject = f"🌱 Time to Hydrate Your {plant_name}!"
+                                    msg_plain = (
+                                        f"Dear {customer_name},\n\n"
+                                        f"Good morning! Just a gentle reminder that it's time to water your {plant_name}.\n\n"
+                                    )
+                                    msg_html = (
+                                       f"<b>Dear {customer_name},</b><br><br>"
+                                       f"Time to refresh your plants. A little water brings them to life.<br>"
+                                        f"Good morning! Just a gentle reminder that it's time to water your <b>{plant_name}</b>.<br><br>"
+                                        "<b>💧 Watering Tip:</b><ul>"
+                                        "<li>Use the recommended amount of water for your plant</li>"
+                                        "<li>Ensure proper drainage to avoid overwatering</li>"
+                                        "<li>Check soil moisture by touching the top 1-2 inches</li>"
+                                        "</ul>"
+                                        "Your plant will thrive with this morning hydration!<br><br>"
+                                        "<b>With green regards,<br>"
+                                        "The AGRO BUILD Team</b><br><br>"
+                                        "Contact Info <br>"
+                                        "B-42 AKRITIGARDEN, NEHRUNAGAR<br>"
+                                        "AHMEDABAD, 380015<br>"
+                                        "📞 8128383925<br>"
+                                        "✉️ shopmulti9859@gmail.com<br>"
+                                    )
+                                else:
+                                    subject = f"🌿 Evening Care for Your {plant_name}"
+                                    msg_plain = (
+                                        f"Hello {customer_name},\n\n"
+                                        f"Don't forget to give your 🌿{plant_name} some evening love!\n\n"
+                                    )
+                                    msg_html = (
+                                        f"<b>Hello {customer_name},</b><br><br>"
+                                        f"Time to refresh your plants. A little water brings them to life.<br>"
+                                        f"Don't forget to give your <b>{plant_name}</b> some evening love!<br><br>"
+                                        "<b>🌻 Best Practices:</b><ul>"
+                                        "<li>Water at the base to keep leaves dry</li>"
+                                        "<li>Use room-temperature water</li>"
+                                        "<li>Adjust quantity based on seasonal needs</li>"
+                                        "</ul>"
+                                        "Thank you for nurturing your green companion!<br><br>"
+                                        "<b>Warmly,<br>AGRO BUILD</b><br><br>"
+                                        "Contact Info <br>"
+                                        "B-42 AKRITIGARDEN, NEHRUNAGAR<br>"
+                                        "AHMEDABAD, 380015<br>"
+                                        "📞 8128383925<br>"
+                                        "✉️ shopmulti9859@gmail.com<br>"   
+                                    )
+                                # Send the email
+                                send_mail(
+                                    subject=subject,
+                                    message=msg_plain,
+                                    from_email=None,
+                                    recipient_list=[reminder.gmail],
+                                    html_message=msg_html,
+                                )
+                                # Also create website notification
+                                NotificationHistory.objects.create(
+                                    user=reminder.user,
+                                    product=product,
+                                    message=msg_plain
+                                )
+                                reminder.last_sent = now
+                                reminder.save()
                                 break
-                    if should_send:
-                        notified_products.append(product.P_name)
-                        # Send Gmail notification
-                        msg_plain = (
-                            "Time to refresh your plants. A little water brings them to life.\n"
-                            f"🌿 Time to water your {product.P_name} \n"
-                        )
-                        msg_html = (
-                            "<b>Time to refresh your plants. A little water brings them to life.</b><br>"
-                            f"🌿 Time to water your {product.P_name} <br>"
-                        )
-                        send_mail(
-                            subject="Watering Reminder",
-                            message=msg_plain,
-                            from_email=None,
-                            recipient_list=[reminder.gmail],
-                            html_message=msg_html,
-                        )
-                        # Also create website notification
-                        NotificationHistory.objects.create(
-                            user=reminder.user,
-                            product=product,
-                            message=msg_plain
-                        )
-                        reminder.last_sent = now
-                        reminder.save()
